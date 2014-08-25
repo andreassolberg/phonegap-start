cb9e47caf53bbab2e0ad1a2963f9e25735598f9f
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2014-08-25 21:58:44 +0200

    Adding jso, jquery, requirejs, and add some init test

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,44 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+define(function(require, exports, module) {
 
-        console.log('Received Event: ' + id);
-    }
-};
+    // var $ = require('jquery');
+
+    var JSO = require('../jso/jso');
+    console.log("READY (1)");
+
+    var onReady = function() {
+
+        console.log("on ready");
+        var jso = new JSO({
+            providerId: "feideconnect",
+            client_id: "42934c73-6fae-4507-92a4-c67f87923aa9",
+            redirect_uri: "http://localhost/~andreas/phonegap-start/www/",
+            authorization: "https://auth.uwap.uninettlabs.no/oauth/authorization"
+        });
+
+
+        jso.callback();
+
+        jso.ajax({
+            url: "https://api.uwap.uninettlabs.no/userinfo",
+            oauth: {
+                scopes: {
+                    request: ["userinfo", "longterm"],
+                    require: ["userinfo"]
+                }
+            },
+            dataType: 'json',
+            success: function(data) {
+                console.log("Response (google):");
+                console.log(data);
+                $(".loader-hideOnLoad").hide();
+            }
+        });
+
+
+    };
+    document.addEventListener('deviceready', onReady, false);
+ 
+
+});
+
